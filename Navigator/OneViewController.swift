aac15c505d0cb4114500958f04515530464eeb96
huijing0024 <huijing0024@gmail.com>
2017-10-10 11:57:33 -0700

    update

---
+++ Navigator/OneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OneViewController.swift
+//  Navigator
+//
+//  Created by Hui Jing on 10/9/17.
+//  Copyright © 2017 HuiJing. All rights reserved.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "ONE"
+    }
+
+}
---
+++ Navigator/TwoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TwoViewController.swift
+//  Navigator
+//
+//  Created by Hui Jing on 10/9/17.
+//  Copyright © 2017 HuiJing. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Two"
+      
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
